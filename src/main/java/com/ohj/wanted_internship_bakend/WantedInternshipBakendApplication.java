c65d9d7f26556034adc177870a5a5fb383e1b2e8
hyujikoh <dvum0045@gmail.com>
2023-08-07 16:12:13 +0900

    enhancement - 초기 yaml 파일 정보 작성

--- src/main/java/com/ohj/wanted_internship_bakend/WantedInternshipBakendApplication.java
+++ src/main/java/com/ohj/wanted_internship_bakend/WantedInternshipBakendApplication.java
@@ -2,8 +2,10 @@ package com.ohj.wanted_internship_bakend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WantedInternshipBakendApplication {
 
     public static void main(String[] args) {
