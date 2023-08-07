da9170a2be73ce892f0e2de6158501e9e4afbd77
hyujikoh <dvum0045@gmail.com>
2023-08-07 15:39:12 +0900

    fix

---
+++ src/main/java/com/ohj/wanted_internship_bakend/WantedInternshipBakendApplication.java
@@ -0,0 +1,13 @@
+package com.ohj.wanted_internship_bakend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedInternshipBakendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedInternshipBakendApplication.class, args);
+    }
+
+}
