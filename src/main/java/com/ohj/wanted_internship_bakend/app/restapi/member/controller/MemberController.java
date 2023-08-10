280c06e3fe341a332117be6d6ca7a20d638d8e5c
hyujikoh <dvum0045@gmail.com>
2023-08-10 09:51:11 +0900

    enhancement - member 도메인 엔드포인트 변경

--- src/main/java/com/ohj/wanted_internship_bakend/app/restapi/member/controller/MemberController.java
+++ src/main/java/com/ohj/wanted_internship_bakend/app/restapi/member/controller/MemberController.java
@@ -7,10 +7,7 @@ import com.ohj.wanted_internship_bakend.app.restapi.member.domain.Member;
 import com.ohj.wanted_internship_bakend.app.restapi.member.domain.MemberReq;
 import com.ohj.wanted_internship_bakend.app.restapi.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -20,6 +17,7 @@ import javax.validation.Valid;
  */
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
 
